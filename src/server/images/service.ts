c93d8931065350a559553cffae97e3352c03fc05
alanmoraales <alanmoraales845@gmail.com>
2025-02-22 02:34:22 -0600

    fix image service typing and returns

--- src/server/images/service.ts
+++ src/server/images/service.ts
@@ -2,24 +2,21 @@ import { eq } from "drizzle-orm";
 import db from "@server/database/client";
 import { images } from "@server/database/schema";
 
-const imageRepository = {
-  insert: db.insert(images),
-  update: db.update(images),
-};
-
 const createOne = async (image: typeof images.$inferInsert) => {
-  const newImage = await imageRepository.insert.values(image);
-  return newImage;
+  const newImage = await db.insert(images).values(image).returning();
+  return newImage[0]!;
 };
 
 const updateOne = async (
   id: number,
   image: Partial<typeof images.$inferInsert>
 ) => {
-  const updatedImage = await imageRepository.update
+  const updatedImage = await db
+    .update(images)
     .set(image)
-    .where(eq(images.id, id));
-  return updatedImage;
+    .where(eq(images.id, id))
+    .returning();
+  return updatedImage[0]!;
 };
 
 const imageService = {
