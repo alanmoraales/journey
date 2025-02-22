666b7f558ca1cb205115366276af5e955c1d19d8
alanmoraales <alanmoraales845@gmail.com>
2025-02-22 01:29:29 -0600

    move db client declaration

---
+++ src/server/database/client.ts
@@ -0,0 +1,8 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import environment from "@server/environment";
+
+const sql = neon(environment.database.url);
+const db = drizzle(sql);
+
+export default db;
--- src/server/jstack.ts
+++ src/server/jstack.ts
@@ -1,25 +1,10 @@
 import { jstack } from "jstack";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import environment from "@server/environment";
 
 export const j = jstack.init();
-
-/**
- * Type-safely injects database into all procedures
- *
- * @see https://jstack.app/docs/backend/middleware
- */
-const databaseMiddleware = j.middleware(async ({ c, next }) => {
-  const sql = neon(environment.database.url);
-  const db = drizzle(sql);
-
-  return await next({ db });
-});
 
 /**
  * Public (unauthenticated) procedures
  *
  * This is the base piece you use to build new queries and mutations on your API.
  */
-export const publicProcedure = j.procedure.use(databaseMiddleware);
+export const publicProcedure = j.procedure;
