deda5647889bcb22f93a7a563487b7427c7e96ae
alanmoraales <alanmoraales845@gmail.com>
2025-02-22 02:34:09 -0600

    remove relativeSrc

--- src/server/database/schema.ts
+++ src/server/database/schema.ts
@@ -3,7 +3,6 @@ import { pgTable, serial, text, timestamp, numeric } from "drizzle-orm/pg-core";
 export const images = pgTable("images", {
   id: serial("id").primaryKey(),
   src: text("src").notNull(),
-  relativeSrc: text("relativeSrc").notNull(),
   bucketKey: text("bucketKey").notNull(),
   width: numeric("width"),
   height: numeric("height"),
