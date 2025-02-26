659720f99e2bbd8757ae4aa2767beec2d9ff3da5
alanmoraales <alanmoraales845@gmail.com>
2025-02-25 19:03:03 -0600

    update image schema

--- src/server/database/schema.ts
+++ src/server/database/schema.ts
@@ -4,9 +4,9 @@ export const images = pgTable("images", {
   id: serial("id").primaryKey(),
   src: text("src").notNull(),
   bucketKey: text("bucketKey").notNull(),
-  width: numeric("width"),
-  height: numeric("height"),
-  placeholderBase64: text("placeholderBase64"),
-  placeholderCss: text("placeholderCss"),
+  width: numeric("width").notNull(),
+  height: numeric("height").notNull(),
+  placeholderBase64: text("placeholderBase64").notNull(),
+  placeholderCss: text("placeholderCss").notNull(),
   createdAt: timestamp("createdAt").defaultNow().notNull(),
 });
