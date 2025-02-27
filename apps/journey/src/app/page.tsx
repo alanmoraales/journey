152f21b49c519f1d37a2b56b7f987b8acb89e389
alanmoraales <alanmoraales845@gmail.com>
2025-02-26 22:50:50 -0600

    remove posts section

--- apps/journey/src/app/page.tsx
+++ apps/journey/src/app/page.tsx
@@ -1,14 +1,12 @@
 import Navbar from "@organisms/Navbar";
 import Flex from "@atoms/Flex";
-import RecentPostsSection from "@organisms/RecentPostsSection/RecentPostsSection";
 import notionService from "@services/notion";
-// import RecentPrintsSection from "@organisms/RecentPrintsSection/RecentPrintsSection";
+import RecentPrintsSection from "@organisms/RecentPrintsSection/RecentPrintsSection";
 // import FeaturedPrintSection from "@organisms/FeaturedPrintSection/FeaturedPrintSection";
 import mixpanelService from "@services/mixpanel";
 
 const Home = async () => {
-  const posts = await notionService.getPosts();
-  // const prints = await notionService.getPrints();
+  const prints = await notionService.getPrints();
   // const featuredPrint = await notionService.getFeaturedPrint();
   mixpanelService.trackEnterPage("Home");
 
@@ -21,8 +19,7 @@ const Home = async () => {
            * Temporary disabled
            */}
           {/* <FeaturedPrintSection print={featuredPrint} /> */}
-          {/* <RecentPrintsSection prints={prints} /> */}
-          <RecentPostsSection posts={posts} />
+          <RecentPrintsSection prints={prints} />
         </Flex>
       </main>
     </Flex>
